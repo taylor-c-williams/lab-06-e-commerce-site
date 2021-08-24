fc6536aeda1898b63bdf8a0e67c416ad38bc86d5
taylor-c-williams <taylor.callan.williams@gmail.com>
2021-08-24 16:13:29 -0700

    getgrandtotal passing

--- test/render-table-row.test.js
+++ test/render-table-row.test.js
@@ -21,4 +21,28 @@ test('renderTableRow should take in a cart item  and render the correct HTML', (
     expect.equal(actual.outerHTML, expected);
 });
 
+test('getGrandTotal should take in item quantity totals and calculate a grand total', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 24;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = getGrandTotal([
+        { id: 1, price: 7 },
+        { id: 2, price: 2 },
+        { id: 3, price: 5 }
+    ],
+    [
+        { id: 2, quantity: 2 },
+        { id: 3, quantity: 4 }
+    ]);
+    
+
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
 
