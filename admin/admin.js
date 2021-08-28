390868eeb296c6e56cfc8dc1aef6538609855a0b
taylor-c-williams <taylor.callan.williams@gmail.com>
2021-08-27 17:07:57 -0700

    array fix on adminjs

--- admin/admin.js
+++ admin/admin.js
@@ -5,14 +5,14 @@ const form = document.getElementById('add-new-product');
 form.addEventListener('submit', (event) =>{
     event.preventDefault();
     const data = new FormData(form);
-    const newProduct = {
+    const newProduct = [{
         id: data.get('id'),
         name: data.get('name'),
         image: data.get('image'),
         description: data.get('description'),
         genre: data.get('genre'),
         price: data.get('price'),
-    };
+    }];
     addProduct(newProduct);
     form.reset(); 
     alert(`Your items have been added to the inventory for eternity`);
